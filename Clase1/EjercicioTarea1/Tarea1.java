a2ea62933c2ed0072ef615f18aa2d2f4f3d9a37f
iDeadtroll <javielproductor@gmail.com>
2023-10-03 08:30:21 +0200

    Tarea2 V1

--- Clase1/EjercicioTarea1/Tarea1.java
+++ Clase1/EjercicioTarea1/Tarea1.java
@@ -3,24 +3,24 @@ package EjercicioTarea1;
 public class Tarea1 {
     public static void main(String args[]) {
         // Ejercicio 1
-        funciones s = new funciones();
+        funciones1 s = new funciones1();
         int sum = s.sumaRecursiva(4);
         System.out.println(sum);
 
         // Ejercicio 2
         int[] array = {4,1,5,-1,10,3,-2};
-        funciones m = new funciones();
+        funciones1 m = new funciones1();
         int menor = m.valMenor(array, 0);
         System.out.println(menor);
 
         // Ejercicio 3
-        funciones sr = new funciones();
+        funciones1 sr = new funciones1();
         int sumArr = sr.sumArr(array, array.length);
         System.out.println(sumArr);
 
         // Ejercicio 4
         String palabra = "reconocer";
-        funciones p = new funciones();
+        funciones1 p = new funciones1();
         boolean esPalindroma = p.Palindroma(palabra);
         if (esPalindroma) {
             System.out.println("'" + palabra + "'" + " es un palíndromo");
@@ -29,14 +29,14 @@ public class Tarea1 {
         }
 
         // Ejercicio 5
-        funciones c = new funciones();
+        funciones1 c = new funciones1();
         int[] a = {1, 3, 2, 3, 1, 1, 0,1};
         int x = 1;
         int cantidad = c.contarX(a, x, 0, 0);
         System.out.println("La cantidad de elementos que coinciden con " + x + " es: " + cantidad);
 
         // Ejercicio Extra 1
-        funciones b = new funciones();
+        funciones1 b = new funciones1();
         int decimal=0;
         System.out.println("Número decimal: " + decimal);
         System.out.print("Equivalente binario: ");
@@ -44,7 +44,7 @@ public class Tarea1 {
 
         // Ejercicio Extra 2
 
-        funciones h = new funciones();
+        funciones1 h = new funciones1();
         h.hanoi(3, "Torre1", "Torre2", "Torre3");
     }
 }
